5c49af5cd91bcc6233cfa5a8eaa5fe17af894d03
Pedro Pinheiro <phgp25@gmail.com>
2022-06-07 21:05:15 -0300

    add screen signin

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-
 import React from 'react';
 import { useFonts, Roboto_300Light, Roboto_400Regular,Roboto_500Medium} from '@expo-google-fonts/roboto'
 import AppLoading from 'expo-app-loading';
 import {ThemeProvider} from 'styled-components/native'
+
 import theme from './src/theme';
+import { Signin } from './src/screens/Signin';
+import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
 
 export default function App() {
 
@@ -19,7 +22,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-     
+      <StatusBar style='light' translucent backgroundColor='transparent' />
+      <Signin></Signin>
     </ThemeProvider>
   );
 }
